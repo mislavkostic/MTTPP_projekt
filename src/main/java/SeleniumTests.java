1343e1e485104b8b75268e88071e2758f169f326
mkostic21 <mislav.kostic@gmail.com>
2022-01-14 11:49:44 +0100

    Implemented register test + refactor

--- src/main/java/SeleniumTests.java
+++ src/main/java/SeleniumTests.java
@@ -10,19 +10,21 @@ import org.testng.annotations.Test;
 public class SeleniumTests{
     //-------------------Global Variables-----------------------------------
     public WebDriver driver;
+    public Persona testPersona;
 
     @BeforeMethod
     public void setupTests(){
         String testURL = "http://www.google.com";
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
+        testPersona = new Persona();
 
         driver.get(testURL);
         driver.findElement(By.xpath("//*[contains(text(), 'Slažem se')]")).click();
     }
 
     @Test
-    public void google_result() throws InterruptedException{
+    public void googleResultTest() throws InterruptedException{
         String expected = "OpenCart Demo";
 
         WebElement searchTextBox = driver.findElement(By.name("q"));
@@ -34,7 +36,7 @@ public class SeleniumTests{
     }
 
     @Test
-    public void homepage_load() throws InterruptedException{
+    public void homepageTest() throws InterruptedException{
         String expected = "Your Store";
         String title;
 
@@ -44,15 +46,47 @@ public class SeleniumTests{
         Assert.assertEquals(title, expected);
     }
 
+    @Test
+    public void registerTest() throws InterruptedException {
+        driver.navigate().to("https://demo.opencart.com/");
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a")).click();
+
+        //registration form
+        driver.findElement(By.id("input-firstname")).sendKeys(testPersona.firstName);
+        driver.findElement(By.id("input-lastname")).sendKeys(testPersona.lastName);
+        driver.findElement(By.id("input-email")).sendKeys(testPersona.email);
+        driver.findElement(By.id("input-telephone")).sendKeys(testPersona.phoneNumber);
+        driver.findElement(By.id("input-password")).sendKeys(testPersona.password);
+        driver.findElement(By.id("input-confirm")).sendKeys(testPersona.password);
+        driver.findElement(By.name("agree")).click();
+        driver.findElement(By.xpath("//input[@value=\"Continue\"]")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/a")).click();
+        Thread.sleep(800);
+
+        //logout
+        driver.findElement(By.xpath("//*[@id=\"column-right\"]/div/a[13]")).click();
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/a")).click();
+        Thread.sleep(1122);
+    }
+
+    @Test
+    public void loginTest(){
+
+    }
+
+    @Test
+    public void addToCartTest(){
+
+    }
+
     @AfterMethod
     public void teardownTest(){
         driver.quit();
     }
 
-
-    //TODO: register test
     //TODO: login test
-    //TODO: browse shop test
     //TODO: add to shopping cart test
     //TODO: README.md documentation
     //TODO: refactor
